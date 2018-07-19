243799dd29e5c1b180edfb7cfc584f0933bc5894
Santiago Ferreira <san650@gmail.com>
2018-07-18 23:53:08 -0300

    Group log messages by phoenix's request id
    Also pretty print phoenix specific log messages

--- lib/pretty_print_formatter/default.ex
+++ lib/pretty_print_formatter/default.ex
@@ -1,4 +1,9 @@
 defmodule PrettyPrintFormatter.Default do
+  @moduledoc """
+  Default formatter used for all messages that we're not going to format
+  """
+
+  # Catch everything that we don't know how to handle
   def run(message) do
     message
   end
